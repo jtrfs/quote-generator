e80279daee8f03ac65ff21f573b7fb61a77feac3
Jan Varmuza <jan.varmuza@gmail.com>
2022-01-15 18:11:23 +0100

    Use js and api to display the quotes, add event listeners for the buttons

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+// html elements
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 const newQuote = () => {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 };
 
 // GET QUOTES FROM API
@@ -16,6 +34,17 @@ const getQuotes = async () => {
     // catching error here, but not in this project
   }
 };
+
+// tweet quote
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+  console.log("hello");
+};
+
+// event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // ON LOAD - APPLICATION STARTS/IS LOADED...
 getQuotes();
